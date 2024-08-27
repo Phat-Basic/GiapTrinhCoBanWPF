b4fd8835c8f74319f37bf91a9be39937ad9a103c
Phat-Basic <phattranlikedata@gmail.com>
2024-08-27 10:34:31 +0700

    Update MonHoc, it list and GiaoVien class

---
+++ Lab2_VD2/GiaoVien.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_VD2
+{
+    public class GiaoVien
+    {
+        public string MaSo { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public 
+    }
+}
